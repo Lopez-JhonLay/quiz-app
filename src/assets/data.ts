cd7edce163db3c9baae4772ccbb91f7945cc79e4
Jhon Lay Lopez <jhonlaylopez000@gmail.com>
2024-10-13 22:34:41 +0800

    create quiz categories sample data

---
+++ src/assets/data.ts
@@ -0,0 +1,7 @@
+export const quizCategories = [
+  { "id": 1, "name": "Science" },
+  { "id": 2, "name": "History" },
+  { "id": 3, "name": "Geography" },
+  { "id": 4, "name": "Movies" },
+  { "id": 5, "name": "Music" }
+]
